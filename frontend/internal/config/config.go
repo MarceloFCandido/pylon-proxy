b7d067fb43fe5e708476ae57249c07de9dc2274b
Marcelo F. Cândido <fcandido.marcelo@gmail.com>
2025-06-26 14:25:34 -0300

    Allowing the proxy URL to be configurable

--- frontend/internal/config/config.go
+++ frontend/internal/config/config.go
@@ -1,6 +1,25 @@
 package config
 
+import "os"
+
 const (
-	ProxyURL        = "http://localhost:8080"
-	ServerPort      = ":8081"
+	DefaultProxyURL   = "http://localhost:8080"
+	DefaultServerPort = ":8081"
 )
+
+var (
+	ProxyURL   string
+	ServerPort string
+)
+
+func init() {
+	ProxyURL = getEnv("PROXY_URL", DefaultProxyURL)
+	ServerPort = getEnv("SERVER_PORT", DefaultServerPort)
+}
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
